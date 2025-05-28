bf0caa1e002c049eedc96bd0d77d2e9117058b88
Shrutakeerti_1111 <shrutakeerti1601@gmail.com>
2025-05-29 03:18:57 +0530

    Update and added the fastapi part in main.py

--- train.py
+++ train.py
@@ -1,13 +1,12 @@
 from ultralytics import YOLO
 
-<<<<<<< HEAD
+
 def train():
     model = YOLO("yolov8n.pt")
     model.train(data="data.yaml", epochs=50)
 
 if __name__ == "__main__":
     train()
-=======
 
 model = YOLO('yolov8n.pt')  
 
@@ -18,4 +17,3 @@ model.train(
     batch=16,           
     name='doors-windows'  
 )
->>>>>>> 2d42543bc4b4a1d498c5eb81cb5440439e98bbc6
